47dd7700e13fd461f6e71771b16dcc0e39c39bcf
lingchen <138715540+lingchen2333@users.noreply.github.com>
2025-11-16 18:23:53 +0000

    add graph

--- chains.py
+++ chains.py
@@ -1,19 +1,22 @@
 import datetime
 
 from dotenv import load_dotenv
+
+load_dotenv()
+
 from langchain_core.messages import HumanMessage
-from langchain_core.output_parsers import JsonOutputToolsParser, PydanticToolsParser
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser,
+)
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 
 from schemas import AnswerQuestion, ReviseAnswer
 
-load_dotenv()
-
-llm = ChatOpenAI(model="gpt-4-turbo-preview")
+llm = ChatOpenAI(model="o4-mini")
 parser = JsonOutputToolsParser(return_id=True)
 parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
-
 
 actor_prompt_template = ChatPromptTemplate.from_messages(
     [
@@ -33,6 +36,7 @@ Current time: {time}
     time=lambda: datetime.datetime.now().isoformat(),
 )
 
+
 first_responder_prompt_template = actor_prompt_template.partial(
     first_instruction="Provide a detailed ~250 word answer."
 )
@@ -50,16 +54,16 @@ revise_instructions = """Revise your previous answer using the new information.
     - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
 """
 
-reviser = actor_prompt_template.partial(
+revisor = actor_prompt_template.partial(
     first_instruction=revise_instructions
 ) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
+
 if __name__ == "__main__":
     human_message = HumanMessage(
-        content="Write about AI-powered SOC / autonomous soc problem domain, "
-        "list startups that do that and raised capital."
+        content="Write about AI-Powered SOC / autonomous soc  problem domain,"
+        " list startups that do that and raised capital."
     )
-
     chain = (
         first_responder_prompt_template
         | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
--- tool_executor.py
+++ tool_executor.py
@@ -1,18 +1,17 @@
-from typing import List
-
 from dotenv import load_dotenv
+
+load_dotenv()
+
 from langchain_core.tools import StructuredTool
 from langchain_tavily import TavilySearch
 from langgraph.prebuilt import ToolNode
 
 from schemas import AnswerQuestion, ReviseAnswer
 
-load_dotenv()
-
 tavily_tool = TavilySearch(max_results=5)
 
 
-def run_queries(search_queries: List[str], **kwargs):
+def run_queries(search_queries: list[str], **kwargs):
     """Run the generated queries."""
     return tavily_tool.batch([{"query": query} for query in search_queries])
 
